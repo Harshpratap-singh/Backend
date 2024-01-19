434f235ea791a2baac3d3921724379ed12472161
Harshpratap-singh <107066198+Harshpratap-singh@users.noreply.github.com>
2024-01-19 12:20:34 +0530

    register ,login,logout added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,32 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name : "coverImage",
-            maxcount : 1
-        }
-    ]),
-    registerUser
-    )
+  upload.fields([
+    //middleware
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxcount: 1,
+    },
+  ]),
+  registerUser
+);
+
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);//get se bhi kar sakte h
 
 
-export default router
+export default router;
